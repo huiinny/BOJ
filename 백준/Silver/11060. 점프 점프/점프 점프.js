82bafea122471eea4eb5c6eab4fa994c8d46d81b
huiinny <134281095+huiinny@users.noreply.github.com>
2025-02-22 14:20:49 +0900

    [Silver II] Title: 점프 점프, Time: 160 ms, Memory: 9852 KB -BaekjoonHub

---
+++ 백준/Silver/11060. 점프 점프/점프 점프.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '/Users/project/BOJ/example.txt';
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = +input[0];
+const jump = input[1].split(" ").map(Number);
+
+function bfs() {
+    if (N === 1) {
+        return 0;
+    }
+
+    const queue = [[0, 0]]; // 현재 위치, cnt
+    const visited = new Array(N).fill(false);
+    visited[0] = true;
+
+    while(queue.length) {
+        const [num, cnt] = queue.shift(); 
+
+        for (let i=num+1; i<=num+jump[num] && i<N; i++) {
+            if (i === N-1) return cnt+1;
+            if (!visited[i]) {
+                visited[i] = true;
+                queue.push([i, cnt+1])
+            }
+
+        }
+    }
+    return -1;
+}
+console.log(bfs());
